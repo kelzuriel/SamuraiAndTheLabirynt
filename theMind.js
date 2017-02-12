c0835c2ad2f17b717683f2e7d3add238e824a516
kelzuriel <kelzuriel@gmail.com>
2017-02-12 17:06:54 +0200

    Improved labyrinth creation. Now you can calculate the shortest path for more than one labyrinth, and find out which one of them is the shortest.

--- theMind.js
+++ theMind.js
@@ -1,35 +1,48 @@
 // the story of the samurai who woke hungover and needed to find his lost katana
 // before his daimio found out and made him cut his guts with his little sword...
 
+function Labyrinth (numberOfLines, numberOfColumns, wallsPerRow, startY, startX, goalY, goalX) {
+  this.numberOfLines = numberOfLines;
+  this.numberOfColumns = numberOfColumns;
+  this.wallsPerRow = wallsPerRow;
+  this.startY = startY;
+  this.startX = startX;
+  this.goalY = goalY;
+  this.goalX = goalX;
+};
 
-var numberOfLines = 3;
-var numberOfColumns = 10;
+//EXAMPLE:
+// var numberOfLines = 3;
+// var numberOfColumns = 10;
 
-var wallsPerRow = {0: [0,1,2,3,4,5,6,7,8,9], 1: [0,7,8,9], 2: [0,1,2,3,4,5,6,7,8,9]};
+// var wallsPerRow = {0: [0,1,2,3,4,5,6,7,8,9], 1: [0,7,8,9], 2: [0,1,2,3,4,5,6,7,8,9]};
 
-var startY = 1;
-var startX = 1;
+// var startY = 1;
+// var startX = 1;
 
-var goalY = 1;
-var goalX = 6;
+// var goalY = 1;
+// var goalX = 6;
 
-(function() {
-  var labyrinth = new Array(numberOfLines);
+var firstLabyrinth = new Labyrinth(3, 10, {0: [0,1,2,3,4,5,6,7,8,9], 1: [0,7,8,9], 2: [0,1,2,3,4,5,6,7,8,9]}, 1, 1, 1, 7);
+var secondLabyrinth = new Labyrinth(3, 10, {0: [0,1,2,3,4,5,6,7,8,9], 1: [0,7,8,9], 2: [0,1,2,3,4,5,6,7,8,9]}, 1, 1, 1, 4);
+
+Labyrinth.prototype.calculate = function() {
+  var labyrinth = new Array(this.numberOfLines);
   for (var i = 0; i < labyrinth.length; i++) {
-    labyrinth[i] = new Array(numberOfColumns).fill('Empty');
+    labyrinth[i] = new Array(this.numberOfColumns).fill('Empty');
   };
 
-  var keys = Object.keys(wallsPerRow);
+  var keys = Object.keys(this.wallsPerRow);
 
   for(var i = 0; i < keys.length; i++) {
-    var columns = wallsPerRow[keys[i]];
+    var columns = this.wallsPerRow[keys[i]];
     for(var j = 0; j < columns.length; j++) {
       labyrinth[keys[i]][columns[j]] = "Wall";
     };
   };
 
-  labyrinth[startY][startX] = "Samurai";
-  labyrinth[goalY][goalX] = "Katana";
+  labyrinth[this.startY][this.startX] = "Samurai";
+  labyrinth[this.goalY][this.goalX] = "Katana";
 
   var calculateShortestPath = function([startY, startX], labyrinth) {
     var dft = startY;
@@ -52,6 +65,7 @@ var goalX = 6;
         var newPosition = exploreInDirection(currentPosition, directions[dir], labyrinth);
         if(newPosition.status == 'Katana') {
           return newPosition;
+
         } else if(newPosition.status == 'Valid') {
           rememoriseList.push(newPosition);
         };
@@ -65,7 +79,7 @@ var goalX = 6;
     var dft = position.distanceFromTop;
     var dfl = position.distanceFromLeft;
 
-    if(dfl < 0 || dfl >= numberOfColumns || dft < 0 || dft >= numberOfLines) {
+    if(dfl < 0 || dfl >= this.numberOfColumns || dft < 0 || dft >= this.numberOfLines) {
       return 'Invalid';
     } else if(labyrinth[dft][dfl] == 'Katana') {
       return 'Katana';
@@ -108,5 +122,11 @@ var goalX = 6;
     return newPosition;
   };
 
-  console.log(calculateShortestPath([startY,startX], labyrinth));
-})(this);
+  var paths = calculateShortestPath([this.startY,this.startX], labyrinth);
+  console.log(paths);
+  return paths.route.length;
+
+};
+
+var first = Math.min(firstLabyrinth.calculate(), secondLabyrinth.calculate());
+console.log("Shortest path is achieved in " + first + " moves.");
